97023709edbf11bdae79c444db0f29f4a16059f4
Mandeep <montytathger@gmail.com>
2024-03-27 10:37:46 +0530

    add qr code sheet

--- src/screens/profile/components/QRCode/QRCode.js
+++ src/screens/profile/components/QRCode/QRCode.js
@@ -1,8 +1,9 @@
-import { View } from "native-base";
+import { Image, View } from "native-base";
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const QRCodeScreen = () => {
-  return <View>
-
+  return <View >
+    <Image style={{width: 300, height: 300}} source={{uri: "https://img.freepik.com/premium-vector/qr-code-sample-smartphone-scanning-qr-code-icon-flat-design-stock-vector-illustration_550395-108.jpg" }} alt="test" size="xl" />
   </View>
 }
 
